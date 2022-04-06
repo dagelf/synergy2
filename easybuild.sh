00c1140fd31d235e65ce527b4e6f58da60e3b167
dagelf <coenraad@wish.org.za>
2022-04-06 10:22:32 +0200

    Added logging

--- easybuild.sh
+++ easybuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# All-in-one script to easily build any package in docker
+# All-in-one script to easily create a Dockerfile and build an image to build any package in docker
 # and to create the neccessary Dockerfiles and build scripts with the right dependencies - and to make finding them easier
 # - without cluttering your host system with unnecessary packages or build tools
 # you can keep editing this file and rerun it after every change... rm -rf src if you want to start build from scratch
@@ -8,6 +8,15 @@
 # ...how about updating the build systems eg making ./configure package- and docker aware...?
 #
 # Synergy2 1.8.8 todo: upgrade libssl
+
+[[ ! -z $1 ]] && { Y="-$1"; } # first parameter is easybuild-imagename # fixme consider using the current directory name
+                              # then the workflow would be to copy just easybuild.sh and code tar files to a new directory 
+                              # to use it in other projects. Alternatively use the script name, then it can just be copied and edited
+
+# Log output
+exec 1> >(tee -a log.easybuild) 2> >(tee -a err.easybuild 2>&1)  # these don't clutter autocompletion.
+echo -e "\n\n--- Easybuild at `date` ---" | tee /dev/stderr
+
 which docker >/dev/null||(echo Install docker first: sudo apt-get install docker.io; exit)
 D="sudo docker"; docker ps -aq 2>/dev/null && D="docker"
 
@@ -15,11 +24,11 @@ D="sudo docker"; docker ps -aq 2>/dev/null && D="docker"
 A="build-essential cmake libcurl4-gnutls-dev libxi-dev libxkbcommon-x11-dev libxtst-dev libxrandr-dev libxinerama-dev unzip" 
 # libssl-dev # needs some code changes
 
-CPUS=`grep -c ^processor /proc/cpuinfo`; [ $CPUS -gt 1 ] && M=-j$CPUS  #fixme remove and rethink this if building for dockerhub or another registry
+CPUS=`grep -c ^processor /proc/cpuinfo`; [ $CPUS -gt 1 ] && M=-j$CPUS 
 
 # this just either apt gets everything in one line, or in separate lines (for quicker package-finding by utilizing docker caches)
-G="apt-get install -y"; L="   "; E="\ "; H=" && $G \\"
-#C=""; L="RUN $G"; E=""; Y="x"; H="# fixme" # comment out this line to make a portable Dockerfile suitable for dockerhub... when you've found all the dependencies
+G="DEBIAN_FRONTEND=noninteractive apt-get install -y"; L="   "; E="\ "; H=" && $G \\" 
+#CPUS=1; C=""; L="RUN $G"; E=""; Y="$Y-dockerhub"; H="# fixme" # comment out this line to make a portable Dockerfile suitable for dockerhub, after you've found all the dependencies
 for B in $A; do 
   C=$(cat <<END
 $C 
@@ -44,19 +53,22 @@ END
 $D build -t $I - < Dockerfile
 
 echo Setting up src/
+[[ -e src ]] || mkdir src
 cp -v $F src/ # you can change the build script here  ---------------------------- dynamic part, if you rm -rf src/ # remove -e to be more lenient
 cat > src/inside-docker.sh <<EOF
 #!/bin/bash -e
-cd /usr/src
+cd /usr/src 
 [ -e target ] || { mkdir target && tar -zxvf $F -C target; }
 cd target/*
+echo Building openssl-1.0.2
 echo ================ THIS VERSION OF OPENSSL IS INSECURE RATHER PATCH THE CODE FOR NEWER SSL ==========================
+echo If it fails, just re-run $0
+sleep 3
 [ -e ext/openssl ] || (cd ext; for a in gmock-1.6.0 gtest-1.6.0; do unzip -o \$a.zip -d \$a; done; for a in *.tar.gz; do tar -zxvf \$a; done; ln -s openssl-1.0.2 openssl; rm openssl/Makefile)
 (cd ext/openssl; [ -e Makefile ] || (./config && make clean && make $M); cp lib* /usr/local/lib || (make $M; cp lib* /usr/local/lib); ln -s \`pwd\`/include/openssl /usr/local/include || : )
 sed -i 's@ssl crypto)@ssl crypto dl)@' src/CMakeLists.txt # patch
 ./configure && make $M
 strip /usr/src/target/afzaalace-synergy-stable-builds-c30301e/bin/*
-
 EOF
 chmod +x src/inside-docker.sh
 echo Running temporary container... # if you get an error, remove the --init
@@ -66,6 +78,7 @@ $D
 echo Done!
 echo src/target/*/bin
 ls -l src/target/*/bin/
+shasum src/target/*/bin/*
 
 echo Making the container interactive...
 cp src/inside-docker.sh src/build.sh
---
+++ inspect.sh
@@ -0,0 +1,1 @@
+docker run --rm --init -it -v /usr/src/github/dagelf/synergy2/src:/usr/src easybuild
